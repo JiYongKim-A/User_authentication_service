90368d679ee89ece04dca608cbf7868c56680fbf
JiYongKim <colorful8315@gmail.com>
2022-11-16 18:35:50 +0900

    chore (Memeber) : Add member properties comment

--- src/main/java/com/study/authentication/domain/member/Member.java
+++ src/main/java/com/study/authentication/domain/member/Member.java
@@ -6,17 +6,23 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 public class Member {
+
+    // 회원 관리 번호
     private Long manageSeq;
 
+    // 회원 ID
     @NotEmpty
     private String id;
 
+    // 회원 PW
     @NotEmpty
     private String password;
 
+    // 회원 이름
     @NotEmpty
     private String name;
 
+    // 회원 전화 번호
     @NotEmpty
     private String tel;
 
