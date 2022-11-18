0cfb3d5c3be62aca61d31b9725a65b54d70f65a0
JiYongKim <colorful8315@gmail.com>
2022-11-18 18:22:53 +0900

    feat (homeController) : Home controller configuration complete

---
+++ src/main/java/com/study/authentication/controller/home/HomeController.java
@@ -0,0 +1,28 @@
+package com.study.authentication.controller.home;
+
+import com.study.authentication.config.session.SimpleSessionManager;
+import com.study.authentication.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    private final SimpleSessionManager sessionManager;
+
+    // 자체적 SessionManager 사용 방법
+    @GetMapping("/")
+    public String homeV1(HttpServletRequest request, Model model) {
+        if(sessionManager.getSession(request) == null){
+            return "home";
+        }
+        Member member = (Member) sessionManager.getSession(request);
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+}
